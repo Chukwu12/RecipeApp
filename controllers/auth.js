3daba311a3465a69762301ff693093f6df66c423
Okechukwu Egeruoh <39450490+Chukwu12@users.noreply.github.com>
2026-05-26 16:18:30 +0000

    Add sign-out nav links and harden logout handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,15 +47,25 @@ exports.postLogin = (req, res, next) => {
   })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
-    console.log('User has logged out.')
-  });
-  req.session.destroy((err) => {
-    if (err)
-      console.log("Error : Failed to destroy the session during logout.", err);
-    req.user = null;
-    res.redirect("/");
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+
+    // If there is no active session, just redirect safely.
+    if (!req.session) {
+      req.user = null;
+      return res.redirect("/");
+    }
+
+    req.session.destroy((destroyErr) => {
+      if (destroyErr) {
+        console.log("Error : Failed to destroy the session during logout.", destroyErr);
+      }
+      req.user = null;
+      res.redirect("/");
+    });
   });
 };
 
